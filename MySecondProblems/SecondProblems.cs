1f7c72cde05507678cbeb05fc661b4783487f47c
florentinc <florentina.cociota@gfi.com>
2015-10-05 09:08:45 +0300

    Add problems

---
+++ MySecondProblems/SecondProblems.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MySecondProblems
+{
+    public class SecondProblems
+    {
+        public static void Main()
+        {
+            string message = "Oriunde e ca acasa!";
+            int no_c = 4;
+            var coding = new SecondProblems();
+            string messageCoding = coding.CodingMessage(message, no_c);
+            Console.WriteLine(messageCoding);
+            //string deCodingMessage = DeCoding(messageCoding, no_c);
+            //Console.WriteLine(deCodingMessage);
+            float rezult = coding.CalculateCombinations(49, 6);
+            long factorial = coding.Factorial(49);
+
+
+        }
+        public string RemoveSpace(string message)
+        {
+            var messageProcessed = Regex.Replace(message, @"\W|_", string.Empty);
+            return messageProcessed;
+        }
+
+        public string RandomChar(int n)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var stringChars = new char[n];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            //Console.WriteLine(finalString);
+            return finalString;
+        }
+
+        public string CodingMessage(string message, int no_c)
+        {
+            string messageClean = RemoveSpace(message);
+            int no_l = (int)Math.Ceiling((double)messageClean.Length / no_c);
+            int noRandom = no_l * no_c - messageClean.Length;
+            string finalMesage = messageClean + RandomChar(noRandom);
+            string finalCoding = "";
+            for (int i = 0; i < no_l; i++)
+            {
+                for (int j = i; j < finalMesage.Length; j = j + no_l)
+                {
+                    finalCoding += finalMesage[j];
+                }
+            }
+            return finalCoding;
+        }
+        public string DeCoding(string message, int no_c)
+        {
+            int no_l = (int)Math.Ceiling((double)message.Length / no_c);
+            string deCoding = "";
+            for (int i = 0; i < no_c; i++)
+            {
+                for (int j = i; j < message.Length; j = j + no_c)
+                {
+                    deCoding += message[j];
+                    //Console.Write(finalMesage[i]);
+
+                }
+            }
+            return deCoding;
+        }
+        public float CalculateCombinations(int n, int k)
+        {
+            var problems = new SecondProblems();
+            int val = n - k;
+            float rezult = problems.Factorial(n) / (problems.Factorial(k) * problems.Factorial(val));
+            return rezult;
+
+        }
+        public long Factorial(int n)
+        {
+            long rezult = 1;
+            for (long i = 1; i <= n; i++)
+                rezult *= i;
+            return rezult;
+
+        }
+        public float ProbabilityWinLoto(int n, int k)
+        {
+            float probability = 0;
+            probability = CalculateCombinations(n, k) * CalculateCombinations(49 - n, n - k) / CalculateCombinations(49, n);
+            return probability;
+        }
+    }
+}
---
+++ MySecondProblemsTest/MySecondProblemsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MySecondProblems.Tests
+{
+    [TestClass]
+    public class SecondProbemsTests
+    {
+        [TestMethod]
+        public void RemoveSpaceTest()
+        {
+            var problems = new SecondProblems();
+            string myString = problems.RemoveSpace("nicaieri nu este ca acasa");
+            Assert.AreEqual(myString, "nicaierinuestecaacasa");
+        }
+
+        [TestMethod]
+        public void CodingMessageTest()
+        {
+            var problems = new SecondProblems();
+            string messageCoding = problems.CodingMessage("Nicaieri nu este ca acasa", 4);
+            Assert.AreEqual(messageCoding, "NicaierinuecaacasaiT");
+        }
+
+        [TestMethod]
+        public void DeCodingMessagetTest()
+        {
+            var problems = new SecondProblems();
+            string messageDeCoding = problems.DeCoding("NicaierinuecaacasaiT", 4);
+            Assert.AreEqual(messageDeCoding, "NicaierinuecaacasaiT");
+        }
+
+        [TestMethod]
+        public void CalculateCombinationsTest()
+        {
+            var problems = new SecondProblems();
+            float rezult = problems.CalculateCombinations(49, 43);
+            Assert.AreEqual(rezult, 1);
+
+        }
+
+        [TestMethod]
+        public void FactorialTest()
+        {
+            var problems = new SecondProblems();
+            long rezult = problems.Factorial(0);
+            Assert.AreEqual(rezult, 1);
+
+        }
+        [TestMethod]
+        public void ProbabilityTest()
+        {
+            var problems = new SecondProblems();
+            float rezult = problems.ProbabilityWinLoto(6, 6);
+            Assert.AreEqual(rezult, 24);
+
+        }
+
+
+    }
+}
