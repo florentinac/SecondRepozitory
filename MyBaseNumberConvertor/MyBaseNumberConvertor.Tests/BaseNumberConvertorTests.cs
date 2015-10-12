aa3fc227b987a3e6bb37eb178bb7ae8564dc46fa
florentinc <florentina.cociota@gfi.com>
2015-10-12 11:24:54 +0300

    Add method

--- MyBaseNumberConvertor/MyBaseNumberConvertor.Tests/BaseNumberConvertorTests.cs
+++ MyBaseNumberConvertor/MyBaseNumberConvertor.Tests/BaseNumberConvertorTests.cs
@@ -10,8 +10,8 @@ namespace MyBaseNumberConvertor.Tests
         public void NumberConvertorToBaseTest()
         {
             var baseNumberConvertor = new BaseNumberConvertor();
-            var number = baseNumberConvertor.ConvertToBase(128, 2, 12);
-            var correctResult = new byte[] { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+            var number = baseNumberConvertor.ConvertToBase(100, 36, 8);
+            var correctResult = new byte[] { 0, 0, 0, 0, 0, 0, 2, 28 };
             CollectionAssert.AreEqual(correctResult, number);
         }
 
@@ -27,24 +27,93 @@ namespace MyBaseNumberConvertor.Tests
         public void AndOperatorTest()
         {
             var baseNumberConvertor = new BaseNumberConvertor();
-            var numberNumberDigitX = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
-            var numberNumberDigitY = new byte[] { 1, 0, 0, 0, 1, 1, 0, 1 };
-            var correctRezult = new byte[] { 1, 0, 1, 0, 1, 1, 1, 1 };
-            var actualRezult = baseNumberConvertor.AndOperator(numberNumberDigitX, numberNumberDigitY);
-            CollectionAssert.AreEqual(actualRezult, correctRezult);
+            var firestByte = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var secondByte = new byte[] { 1, 0, 0, 0, 1, 1, 0, 1 };
+            var correctResult = new byte[] { 1, 0, 1, 0, 1, 1, 1, 1 };
+            var actualResult = baseNumberConvertor.AndOperator(firestByte, secondByte);
+            CollectionAssert.AreEqual(actualResult, correctResult);
         }
+
         [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AndOperatorExceptionTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var firestByte = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var secondByte = new byte[] { 1, 0, 1, 0, 0, 0, 1, 1, 0, 1 };
+            var result = baseNumberConvertor.AndOperator(firestByte, secondByte);
+        }
+
         public void OrOperatorTest()
         {
             var baseNumberConvertor = new BaseNumberConvertor();
-            var numberNumberDigitX = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
-            var numberNumberDigitY = new byte[] { 1, 0, 0, 0, 1, 1, 0, 1 };
-            var correctRezult = new byte[] { 1, 0, 0, 0, 1, 0, 0, 1 };
-            var actualRezult = baseNumberConvertor.OrOperator(numberNumberDigitX, numberNumberDigitY);
-            CollectionAssert.AreEqual(actualRezult, correctRezult);
+            var firestByte = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var secondByte = new byte[] { 1, 0, 0, 0, 1, 1, 0, 1 };
+            var correctResult = new byte[] { 1, 0, 0, 0, 1, 0, 0, 1 };
+            var actualResult = baseNumberConvertor.OrOperator(firestByte, secondByte);
+            CollectionAssert.AreEqual(actualResult, correctResult);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void OrOperatorExceptionTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var firestByte = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var secondByte = new byte[] { 0, 1, 1, 0, 0, 0, 1, 1, 0, 1 };
+            var result = baseNumberConvertor.OrOperator(firestByte, secondByte);
+        }
 
+        [TestMethod]
+        public void NotOperatorTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var number =        new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var correctResult = new byte[] { 0, 1, 0, 1, 0, 1, 0, 0 };
+            var actualResult = baseNumberConvertor.Not(number);
+            CollectionAssert.AreEqual(actualResult, correctResult);
+        }
+
+        [TestMethod]
+        public void XorOperatorTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var firestByte =    new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var secondByte =    new byte[] { 1, 0, 0, 0, 1, 1, 0, 1 };
+            var correctResult = new byte[] { 1, 1, 0, 1, 1, 0, 0, 1 };
+            var actualResult = baseNumberConvertor.XorOperator(firestByte, secondByte);
+            CollectionAssert.AreEqual(actualResult, correctResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void XorOperatorExceptionTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var firestByte = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var secondByte = new byte[] { 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1 };
+            var actualResult = baseNumberConvertor.XorOperator(firestByte, secondByte);
+        }
+
+        [TestMethod]
+        public void RightHandShiftTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var number = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var correctResult = new byte[] { 1, 1, 0, 1, 0, 1, 0, 1 };
+            var actualResult = baseNumberConvertor.RightHandShift(number,1);
+            CollectionAssert.AreEqual(actualResult, correctResult);
+        }
+
+        [TestMethod]
+        public void LeftHandShiftTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var number =        new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var correctResult = new byte[] { 0, 1, 0, 1, 0, 1, 1, 1 };
+            var actualResult = baseNumberConvertor.LeftHandShift(number, 1);
+            CollectionAssert.AreEqual(actualResult, correctResult);
+        }
     }
 }
 
--- MyBaseNumberConvertor/MyBaseNumberConvertor/BaseNumberConvertor.cs
+++ MyBaseNumberConvertor/MyBaseNumberConvertor/BaseNumberConvertor.cs
@@ -12,7 +12,6 @@ namespace MyBaseNumberConvertor
         {
             var result = new byte[lenght];
             int index = lenght;
-
             while (number > 0)
             {
                 var remainder = number % baseX;
@@ -29,14 +28,6 @@ namespace MyBaseNumberConvertor
             return result;
         }
 
-        public int ConvertFromBase(string numberDigits, int baseX)
-        {
-            var result = 0;
-            for (var i = 0; i < numberDigits.Length; i++)
-                result = result + (int)(int.Parse(numberDigits[i].ToString()) * Math.Pow(baseX, numberDigits.Length - i - 1));
-            return result;
-        }
-
         public int ConvertFromBase(byte[] numberDigits, int baseX)
         {
             var result = 0;
@@ -47,20 +38,87 @@ namespace MyBaseNumberConvertor
         public byte[] AndOperator(byte[] firstBytes, byte[] secondBytes)
         {
             var rezult = new byte[firstBytes.Length];
-            for (var i = 0; i < firstBytes.Length; i++)
-                if (firstBytes[i] == 0 && secondBytes[i] == 0)
-                    rezult[i] = 0;
-                else rezult[i] = 1;
+            if (firstBytes.Length == secondBytes.Length)
+            {
+                for (var i = 0; i < firstBytes.Length; i++)
+                    if (firstBytes[i] == 0 && secondBytes[i] == 0)
+                        rezult[i] = 0;
+                    else rezult[i] = 1;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
             return rezult;
         }
+
         public byte[] OrOperator(byte[] firstBytes, byte[] secondBytes)
         {
-            var rezult = new byte[firstBytes.Length];
-            for (var i = 0; i < firstBytes.Length; i++)
-                if (firstBytes[i] == 0 || secondBytes[i] == 0)
-                    rezult[i] = 0;
-                else rezult[i] = 1;
+            var result = new byte[firstBytes.Length];
+            if (firstBytes.Length == secondBytes.Length)
+            {
+                
+                for (var i = 0; i < firstBytes.Length; i++)
+                    if (firstBytes[i] == 0 || secondBytes[i] == 0)
+                        result[i] = 0;
+                    else result[i] = 1;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            return result;
+        }
+
+        public byte[] Not(byte[] number)
+        {
+            var rezult = new byte[number.Length];
+            for (var i = 0; i < number.Length; i++)
+            {
+                if (number[i] == 0)
+                    rezult[i] = 1;
+                else rezult[i] = 0;
+            }
             return rezult;
+        }
+        public byte[] XorOperator(byte[] firstBytes, byte[] secondBytes)
+        {
+            var rezult = new byte[firstBytes.Length];
+            if (firstBytes.Length == secondBytes.Length)
+            {
+                for (var i = 0; i < firstBytes.Length; i++)
+                    if ((firstBytes[i] == 0 && secondBytes[i] == 0) || (firstBytes[i] == 1 && secondBytes[i] == 1))
+                        rezult[i] = 1;
+                    else rezult[i] = 0;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            return rezult;
+        }
+
+        public byte[] RightHandShift(byte[] number, int count)
+        {
+            var result = new byte[number.Length];
+            for (var i = 1; i <= number.Length - 1; i++)
+            {
+                result[i] = number[i-1];
+            }
+            result[0] = number[number.Length - count];
+            return result;
+        }
+
+        public byte[] LeftHandShift(byte[] number, int count)
+        {
+            var result = new byte[number.Length];
+            for (var i = 0; i < number.Length - 1; i++)
+            {
+                result[i] = number[i + count];
+            }
+            result[number.Length - 1] = number[0];
+            return result;
         }
     }
 }
