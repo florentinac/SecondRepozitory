d3b0c40f88f41081ea6b35371298833b12021509
florentinc <florentina.cociota@gfi.com>
2015-10-12 07:46:11 +0300

    Add method: AndOperator, OrOperator

--- MyBaseNumberConvertor/MyBaseNumberConvertor.Tests/BaseNumberConvertorTests.cs
+++ MyBaseNumberConvertor/MyBaseNumberConvertor.Tests/BaseNumberConvertorTests.cs
@@ -10,18 +10,41 @@ namespace MyBaseNumberConvertor.Tests
         public void NumberConvertorToBaseTest()
         {
             var baseNumberConvertor = new BaseNumberConvertor();
-            var number = baseNumberConvertor.ConvertToBase(11, 2);
-            var correctResult = new byte[] {0,0,0,0,1,0,1,1};
-            CollectionAssert.AreEqual(correctResult,number);
-            //Assert.AreEqual(number, correctResult);
+            var number = baseNumberConvertor.ConvertToBase(128, 2, 12);
+            var correctResult = new byte[] { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+            CollectionAssert.AreEqual(correctResult, number);
         }
 
         [TestMethod]
         public void NumberConvertorFromBaseTest()
         {
             var baseNumberConvertor = new BaseNumberConvertor();
-            var number = baseNumberConvertor.ConvertFromBase("110", 2);
-            Assert.AreEqual(number, 6);
+            var numberdigit = new byte[] { 1, 0, 0, 0, 0, 0, 0, 0 };
+            var number = baseNumberConvertor.ConvertFromBase(numberdigit, 2);
+            Assert.AreEqual(number, 128);
         }
+        [TestMethod]
+        public void AndOperatorTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var numberNumberDigitX = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var numberNumberDigitY = new byte[] { 1, 0, 0, 0, 1, 1, 0, 1 };
+            var correctRezult = new byte[] { 1, 0, 1, 0, 1, 1, 1, 1 };
+            var actualRezult = baseNumberConvertor.AndOperator(numberNumberDigitX, numberNumberDigitY);
+            CollectionAssert.AreEqual(actualRezult, correctRezult);
+        }
+        [TestMethod]
+        public void OrOperatorTest()
+        {
+            var baseNumberConvertor = new BaseNumberConvertor();
+            var numberNumberDigitX = new byte[] { 1, 0, 1, 0, 1, 0, 1, 1 };
+            var numberNumberDigitY = new byte[] { 1, 0, 0, 0, 1, 1, 0, 1 };
+            var correctRezult = new byte[] { 1, 0, 0, 0, 1, 0, 0, 1 };
+            var actualRezult = baseNumberConvertor.OrOperator(numberNumberDigitX, numberNumberDigitY);
+            CollectionAssert.AreEqual(actualRezult, correctRezult);
+        }
+
+
     }
 }
+
--- MyBaseNumberConvertor/MyBaseNumberConvertor/BaseNumberConvertor.cs
+++ MyBaseNumberConvertor/MyBaseNumberConvertor/BaseNumberConvertor.cs
@@ -1,27 +1,31 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
 
 namespace MyBaseNumberConvertor
 {
     public class BaseNumberConvertor
     {
-        public byte[] ConvertToBase(int number, int baseX)
+        public byte[] ConvertToBase(int number, int baseX, int lenght)
         {
-            var result = new byte[8];
-            int index = 7;
-            
-                while (number > 0)
-                {
-                    var remainder = number % baseX;
-                    number = number/baseX;
-                    result[index--] = (byte)(remainder);
-                }
-            
+            var result = new byte[lenght];
+            int index = lenght;
+
+            while (number > 0)
+            {
+                var remainder = number % baseX;
+                number = number / baseX;
+                result[--index] = (byte)(remainder);
+            }
+
+            return result;
+        }
+        public byte[] Add(int number, int lenght)
+        {
+            var result = new byte[lenght];
+            result[--lenght] = (byte)number;
             return result;
         }
 
@@ -29,8 +33,34 @@ namespace MyBaseNumberConvertor
         {
             var result = 0;
             for (var i = 0; i < numberDigits.Length; i++)
-                result = result + (int)(int.Parse(numberDigits[i].ToString())*Math.Pow(baseX, numberDigits.Length-i-1));
+                result = result + (int)(int.Parse(numberDigits[i].ToString()) * Math.Pow(baseX, numberDigits.Length - i - 1));
             return result;
+        }
+
+        public int ConvertFromBase(byte[] numberDigits, int baseX)
+        {
+            var result = 0;
+            for (var i = 0; i < numberDigits.Length; i++)
+                result = result + (int)(numberDigits[i] * Math.Pow(baseX, numberDigits.Length - i - 1));
+            return result;
+        }
+        public byte[] AndOperator(byte[] firstBytes, byte[] secondBytes)
+        {
+            var rezult = new byte[firstBytes.Length];
+            for (var i = 0; i < firstBytes.Length; i++)
+                if (firstBytes[i] == 0 && secondBytes[i] == 0)
+                    rezult[i] = 0;
+                else rezult[i] = 1;
+            return rezult;
+        }
+        public byte[] OrOperator(byte[] firstBytes, byte[] secondBytes)
+        {
+            var rezult = new byte[firstBytes.Length];
+            for (var i = 0; i < firstBytes.Length; i++)
+                if (firstBytes[i] == 0 || secondBytes[i] == 0)
+                    rezult[i] = 0;
+                else rezult[i] = 1;
+            return rezult;
         }
     }
 }
