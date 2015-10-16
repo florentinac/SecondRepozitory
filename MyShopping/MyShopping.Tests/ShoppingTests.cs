71345068e43e87dda5840edb79356320e6a885c9
florentinc <florentina.cociota@gfi.com>
2015-10-16 15:27:19 +0300

    Add Shopping Problem

---
+++ MyShopping/MyShopping.Tests/ShoppingTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShopping;
+
+namespace MyShopping.Tests
+{
+    [TestClass]
+    public class ShoppingTests
+    {
+        public Shopping.ShoppingCart[] Cart = new Shopping.ShoppingCart[]
+        {
+            new Shopping.ShoppingCart {ProducName = "milk", Price = 10},
+            new Shopping.ShoppingCart {ProducName = "yogurt", Price = 13},
+            new Shopping.ShoppingCart {ProducName = "bread", Price = 4},
+            new Shopping.ShoppingCart {ProducName = "apple", Price = 20},
+            new Shopping.ShoppingCart {ProducName = "cico", Price = 5},
+            new Shopping.ShoppingCart {ProducName = "strawberries", Price = 100},
+            new Shopping.ShoppingCart {ProducName = "vegetables", Price = 1},          
+        };
+
+        public Shopping.ShoppingCart[] DeleteCart = new Shopping.ShoppingCart[]
+        {
+            new Shopping.ShoppingCart {ProducName = "milk", Price = 10},
+            new Shopping.ShoppingCart {ProducName = "yogurt", Price = 13},
+            new Shopping.ShoppingCart {ProducName = "bread", Price = 4},
+            new Shopping.ShoppingCart {ProducName = "apple", Price = 20},
+            new Shopping.ShoppingCart {ProducName = "cico", Price = 5},
+            new Shopping.ShoppingCart {ProducName = "vegetables", Price = 1},
+        };
+
+
+        [TestMethod]
+        public void TestToalPriceOfSoppingCart()
+        {
+            var expectedPrice = 283;
+            var acutalPrice = Shopping.GetTotalPaymentOfShoppingCart(Cart);
+            Assert.AreEqual(expectedPrice,acutalPrice);
+
+        }
+
+        [TestMethod]
+        public void CheapestProductFromSoppingCart()
+        {
+            var expectedProduct = "vegetables";
+            var acutalProduct = Shopping.GetTheCheapestProduct(Cart);
+            Assert.AreEqual(expectedProduct, acutalProduct);
+
+        }
+
+        [TestMethod]
+        public void MaxPriceOfProductFromSoppingCart()
+        {
+            var expectedProduct = 119;            
+            var acutalProduct = Shopping.GetMaximPrice(Cart);
+            Assert.AreEqual(expectedProduct, acutalProduct);
+
+        }
+        [TestMethod]
+        public void DeleteProductWhoHeveMaxPriceFromSoppingCart()
+        {
+            var expectedProduct = DeleteCart;
+            var acutalProduct = Shopping.DeleteProduct(Cart);
+            Assert.AreEqual(expectedProduct, acutalProduct);
+
+        }
+
+
+    }
+}
---
+++ MyShopping/MyShopping/Shopping.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopping
+{
+    public class Shopping
+    {
+        public struct ShoppingCart
+        {
+            public string ProducName;
+            public int Price;
+                   
+
+        }
+
+        public static int GetTotalPaymentOfShoppingCart(ShoppingCart[] cart)
+        {
+            var totalPayment = 0;
+            for (var i = 0; i < cart.Length; i++)
+                totalPayment += cart[i].Price;
+
+            return totalPayment;
+        }
+
+        public static string GetTheCheapestProduct(ShoppingCart[] cart)
+        {
+            var cheapestProduct = cart[0].ProducName;
+            var minPrice = cart[0].Price;
+            for (var i=0;i<cart.Length;i++)
+                if (cart[i].Price < minPrice)
+                {                   
+                    minPrice = cart[i].Price;
+                    cheapestProduct = cart[i].ProducName;
+                }
+
+            return cheapestProduct;
+        }
+
+        public static int GetMaximPrice(ShoppingCart[] cart)
+        {
+            var maxPrice = cart[0].Price;
+            for(var i=0;i<cart.Length;i++)
+                if (cart[i].Price > maxPrice)
+                    maxPrice = cart[i].Price;
+            return maxPrice;
+        }
+
+        public static ShoppingCart[] DeleteProduct(ShoppingCart[] cart)
+        {
+            var maxPrice = GetMaximPrice(cart);
+            var newCart = new ShoppingCart[cart.Length-1];
+            for( var i=1;i<cart.Length;i++)
+                if (cart[i].Price != maxPrice)
+                    newCart[i-1] = cart[i];
+            return newCart;
+
+        }
+    }
+}
