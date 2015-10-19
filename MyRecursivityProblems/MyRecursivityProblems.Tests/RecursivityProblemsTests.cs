1680e7db7f69663c07fd5102b1ea17bee70cafad
florentinc <florentina.cociota@gfi.com>
2015-10-19 17:21:45 +0300

    Add RecursivityProblems

---
+++ MyRecursivityProblems/MyRecursivityProblems.Tests/RecursivityProblemsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyRecursivityProblems.Tests
+{
+    [TestClass]
+    public class RecursivityProblemsTests
+    {
+        [TestMethod]
+        public void FibonacciTest()
+        {
+            var expectedRezult = 13;
+            var actualRezult = RecursivityProblems.GetFibonacciNumber(7);
+            Assert.AreEqual(expectedRezult,actualRezult);
+        }
+
+        [TestMethod]
+        public void ReverseStringTest()
+        {
+            var expectedResult = "dcba";
+            var actualResult = RecursivityProblems.ReverseString("abcd");
+            Assert.AreEqual(expectedResult,actualResult);
+        }   
+
+        [TestMethod]
+        public void ReplaceLeterWithStringTest()
+        {
+            var expectedResult = "xxxnxxxnxxx";
+            var actualResult = RecursivityProblems.ReplaceLeterWithString("anana", 'a', "xxx");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void RecursivityTest()
+        {
+            var expectedResult = 6;
+            var actualResult = RecursivityProblems.CalculateRecursiv("+ + + 2.5 3.5 4 7");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ MyRecursivityProblems/MyRecursivityProblems/RecursivityProblems.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecursivityProblems
+{
+    public class RecursivityProblems
+    {
+        public static long GetFibonacciNumber(int n)
+        {
+            if (n == 0) return 0;           
+            if (n == 1) return 1;
+            
+            return GetFibonacciNumber(n - 1) + GetFibonacciNumber(n - 2);
+
+        }
+
+        public static string ReverseString(string s)
+        {
+            if (s.Length == 1||s== string.Empty) return s;         
+            return s[s.Length - 1] + ReverseString(s.Substring(0, s.Length - 1));
+        }
+       
+        public static string ReplaceLeterWithString(string s, char leter, string newleter)
+        {
+            if ((s.Length == 1) && (s[0] == leter))
+                return newleter;
+            if (((s.Length == 1) && (s[0] != leter)) || s == string.Empty)
+                return s;
+            return s[0] != leter
+                ? s[0] + ReplaceLeterWithString(s.Substring(1, s.Length - 1), leter, newleter)
+                : newleter + ReplaceLeterWithString(s.Substring(1, s.Length - 1), leter, newleter);
+
+        }
+
+        public static double CalculateRecursiv(string s)
+        {          
+            var sClean = s.Split(' ');
+            for (int i = 0; i < sClean.Length; i++)
+            {
+                if (sClean[i] != "+")
+                {
+                    var rez = Add(double.Parse(sClean[i]), double.Parse(sClean[i+1]));
+                    return rez +
+                           CalculateRecursiv(string.Concat(string.Concat(s.Substring(0, i - 1), rez.ToString()),
+                               s.Substring(i + 2, s.Length - 1)));
+                }
+            }
+            return 0;
+
+
+
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Substraction(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multiplay(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Division(double x, double y)
+        {
+            return x / y;
+        }
+
+
+
+
+
+    }
+}
