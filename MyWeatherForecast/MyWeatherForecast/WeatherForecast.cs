d3987849080eb73aac7e3561968d6fb20530cd29
florentinc <florentina.cociota@gfi.com>
2015-10-19 08:48:35 +0300

    Add WeatherForcast Problem

---
+++ MyWeatherForecast/MyWeatherForecast/WeatherForecast.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeatherForecast
+{
+    public class WeatherForecast
+    {
+        public struct WeatherDays
+        {
+            public string Month;
+            public string Day;
+            public int MaxTemperature;
+            public int MinTemperature;
+
+        };
+        public static int GetMaxVaule(WeatherDays[] weather)
+        {
+            var maxValue = weather[0].MinTemperature;
+            for (var i = 0; i < weather.Length; i++)
+            {
+                if (weather[i].MaxTemperature >= maxValue)
+                    maxValue = weather[i].MaxTemperature;
+            }
+            return maxValue;
+        }
+
+        public static int GetMinValue(WeatherDays[] weather)
+        {
+            var minValue = weather[0].MaxTemperature;
+            for (var i = 0; i < weather.Length; i++)
+            {
+                if (weather[i].MaxTemperature <= minValue)
+                    minValue = weather[i].MaxTemperature;
+            }
+            return minValue;
+        }
+
+        public static int GetTotalValue(WeatherDays[] weather, string month)
+        {
+            var totalValue = 0;
+            for (var i = 0; i < weather.Length; i++)
+                if (weather[i].Month == month)
+                    totalValue += weather[i].MaxTemperature;
+            return totalValue;
+        }
+
+        public static float GetAveregeTemperature(WeatherDays[] weather, string month)
+        {
+            float averegeTemperature = 0;
+            var index = 0;
+            float totalOfMaxTemperature = GetTotalValue(weather, month);
+            for (var i = 0; i < weather.Length; i++)
+                if (weather[i].Month == month)
+                    index++;
+            averegeTemperature = totalOfMaxTemperature / index;
+            return averegeTemperature;
+        }
+        public static WeatherDays[] AddToArray(ref WeatherDays[] array, WeatherDays newValue)
+        {
+            array = ResizeArray(array, 1);
+            array[array.Length - 1] = newValue;
+            return array;
+        }
+
+        public static WeatherDays[] ResizeArray(WeatherDays[] oldArray, int difference)
+        {
+            var newArray = new WeatherDays[oldArray.Length + difference];
+            for (var i = 0; i < oldArray.Length; i++)
+                newArray[i] = oldArray[i];
+            return newArray;
+        }
+
+        public static WeatherDays[] GetTheHottestDay(WeatherDays[] weather)
+        {
+            var maxOfMaxTemperature = GetMaxVaule(weather);
+            var dayWithMaxTemperature = new WeatherDays[0];
+            for (var i = 0; i < weather.Length; i++)
+                if (weather[i].MaxTemperature == maxOfMaxTemperature)
+                    dayWithMaxTemperature = AddToArray(ref dayWithMaxTemperature, weather[i]);
+            return dayWithMaxTemperature;
+        }
+
+        public static WeatherDays[] GetTheColdexDay(WeatherDays[] weather)
+        {
+            var minOfMaxTemperature = GetMinValue(weather);
+            var dayWithMinOfMaxTemperature = new WeatherDays[0];
+            for (var i = 0; i < weather.Length; i++)
+                if (weather[i].MaxTemperature == minOfMaxTemperature)
+                    dayWithMinOfMaxTemperature = AddToArray(ref dayWithMinOfMaxTemperature, weather[i]);
+            return dayWithMinOfMaxTemperature;
+        }
+
+        public static int GetMaxDifference(WeatherDays[] weather)
+        {
+            var maxDifference = weather[0].MaxTemperature - weather[0].MinTemperature;
+            for (var i = 0; i < weather.Length; i++)
+            {
+                var difference = weather[i].MaxTemperature - weather[i].MinTemperature;
+                if (difference > maxDifference)
+                    maxDifference = difference;
+            }
+            return maxDifference;
+        }
+    }
+}
---
+++ MyWeatherForecast/MyWeatherForecastTests/WeatherForecastTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyWeatherForecast.Tests
+{
+    [TestClass]
+    public class WeatherForecastTests
+    {
+        public WeatherForecast.WeatherDays[] WeatherDays = new WeatherForecast.WeatherDays[]
+        {
+            new WeatherForecast.WeatherDays {Month ="Mai", Day="Sunday", MaxTemperature=20,MinTemperature=15},
+            new WeatherForecast.WeatherDays {Month ="Mai", Day="Monday", MaxTemperature=20,MinTemperature=8},
+            new WeatherForecast.WeatherDays {Month ="March", Day="Tuesday", MaxTemperature=16,MinTemperature=7},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Wednesday", MaxTemperature=15,MinTemperature=10},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Thursday", MaxTemperature=13,MinTemperature=8},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Friday", MaxTemperature=14,MinTemperature=9},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Saturday", MaxTemperature=13,MinTemperature=5}
+        };
+
+        public WeatherForecast.WeatherDays[] HottestDays = new WeatherForecast.WeatherDays[]
+        {
+            new WeatherForecast.WeatherDays {Month ="Mai", Day="Sunday", MaxTemperature=20,MinTemperature=15},
+            new WeatherForecast.WeatherDays {Month ="Mai", Day="Monday", MaxTemperature=20,MinTemperature=8}
+        };
+
+        public WeatherForecast.WeatherDays[] ColdexDays = new WeatherForecast.WeatherDays[]
+        {
+            new WeatherForecast.WeatherDays {Month ="November", Day="Thursday", MaxTemperature=13,MinTemperature=8},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Saturday", MaxTemperature=13,MinTemperature=5}
+        };
+
+        public WeatherForecast.WeatherDays[] NewWeatherDays = new WeatherForecast.WeatherDays[]
+        {
+            new WeatherForecast.WeatherDays {Month ="Mai", Day="Sunday", MaxTemperature=20,MinTemperature=15},
+            new WeatherForecast.WeatherDays {Month ="Mai", Day="Monday", MaxTemperature=20,MinTemperature=8},
+            new WeatherForecast.WeatherDays {Month ="March", Day="Tuesday", MaxTemperature=16,MinTemperature=7},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Wednesday", MaxTemperature=15,MinTemperature=10},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Thursday", MaxTemperature=13,MinTemperature=8},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Friday", MaxTemperature=14,MinTemperature=9},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Saturday", MaxTemperature=13,MinTemperature=5},
+            new WeatherForecast.WeatherDays {Month ="November", Day="Sunday", MaxTemperature=18,MinTemperature=11}
+        };
+
+        [TestMethod]
+        public void MaxDifferenceBetweenTheMaxAndMinTemperature()
+        {
+            var expectedDifference = 12;
+            var actualDifference = WeatherForecast.GetMaxDifference(WeatherDays);
+            Assert.AreEqual(expectedDifference, actualDifference);
+        }
+
+        [TestMethod]
+        public void AddANewWeatherInWeatherDay()
+        {
+            var newWeather = new WeatherForecast.WeatherDays { Month = "November", Day = "Sunday", MaxTemperature = 18, MinTemperature = 11 };
+            var expectedProduct = NewWeatherDays;
+            var actualProduct = WeatherForecast.AddToArray(ref WeatherDays, newWeather);
+            CollectionAssert.AreEqual(expectedProduct, actualProduct);
+        }
+
+        [TestMethod]
+        public void HottestDaysWithSameMaxTemperature()
+        {
+            var expectedProduct = HottestDays;
+            var actualProduct = WeatherForecast.GetTheHottestDay(WeatherDays);
+            CollectionAssert.AreEqual(expectedProduct, actualProduct);
+        }
+
+        [TestMethod]
+        public void ColdexDaysWithSameMaxTemperature()
+        {
+            var expectedProduct = ColdexDays;
+            var actualProduct = WeatherForecast.GetTheColdexDay(WeatherDays);
+            CollectionAssert.AreEqual(expectedProduct, actualProduct);
+        }
+
+        [TestMethod]
+        public void AveregeTemperature()
+        {
+            var expectedProduct = 13.75;
+            var actualProduct = WeatherForecast.GetAveregeTemperature(WeatherDays, "November");
+            Assert.AreEqual(expectedProduct, actualProduct);
+        }
+    }
+}
