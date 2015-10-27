ba530bb71b8440bfdb53074e99a15662313dcd1a
florentinc <florentina.cociota@gfi.com>
2015-10-27 11:00:28 +0200

    Add SortingAlgorithms

---
+++ MySortingAndSearchingAlgorithms/MySortingAndSearching.Tests/SortingAndSearchingAlgorithmsTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySortingAndSearchingAlgorithms;
+
+namespace MySortingAndSearching.Tests
+{
+    [TestClass]
+    public class SortingAndSearchingAlgorithmsTests
+    {
+        [TestMethod]
+        public void QuickSortAscedingTest()
+        {
+            var expectedResult = new char[] {'a', 'b','c', 'd','e','f','m'};
+            var text = new char[] { 'd', 'a', 'b','c', 'f', 'm', 'e' };
+            SortingAndSearchingAlgorithms.QuickSortForOrderAscendingText(text, 0, text.Length-1);
+            CollectionAssert.AreEqual(text,expectedResult);
+        }
+        [TestMethod]
+        public void QuickSortDescendingTest()
+        {
+            var expectedResult = "mfedcba";
+            var text = new char[] { 'd', 'a', 'b', 'c', 'f', 'm', 'e' };
+            var actualResult = SortingAndSearchingAlgorithms.GetDescendingText(text, 0, text.Length - 1);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestMethod]
+        public void BubbleSortAscedingTest()
+        {
+            var expectedResult = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'm' };
+            var text = new char[] { 'a', 'b', 'd', 'c', 'e', 'm', 'f' };
+            var actualResult = SortingAndSearchingAlgorithms.BubbleSort(text);
+            CollectionAssert.AreEqual(actualResult, expectedResult);
+        }
+
+        //[TestMethod]
+        //public void QuickSortThreeWayPartitionTest()
+        //{
+        //    var expectedResult = new string[] { "medium", "medium", "high", "low", "high", "low", "medium" };
+        //    var text = new string[] { "high","high","medium","medium","medium", "low","low"};
+        //    var actualResult = SortingAndSearchingAlgorithms.QuickSortThreeWayPartition(text);
+        //    CollectionAssert.AreEqual(actualResult, expectedResult);
+        //}
+
+        [TestMethod]
+        public void LessStringTest()
+        {           
+            var actualResult = SortingAndSearchingAlgorithms.Less("maria","mariaa");
+            Assert.AreEqual(actualResult, true);
+        }
+
+    }
+}
---
+++ MySortingAndSearchingAlgorithms/MySortingAndSearchingAlgorithms/SortingAndSearchingAlgorithms.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySortingAndSearchingAlgorithms
+{
+    public class SortingAndSearchingAlgorithms
+    {
+
+        public static void QuickSortForOrderAscendingText(char[] text, int left, int right)
+        {           
+            if (text.Length <= 1)
+                return;
+            if (left < right)
+            {
+                var pivot = Partition(text, left, right);
+
+                if (pivot > 1)
+                   QuickSortForOrderAscendingText(text, left, pivot - 1);
+                if (pivot + 1 < right)
+                   QuickSortForOrderAscendingText(text, pivot + 1, right);
+            }           
+        }
+
+        public static int Partition(char[] text,int lowIndex ,int highIndex)
+        {
+            int pivot = text[lowIndex];
+            while (true)
+            {
+                while (text[lowIndex] < pivot)
+                    lowIndex++;
+                while (text[highIndex] > pivot)
+                    highIndex--;
+                if (lowIndex < highIndex)
+                {
+                    Swap(ref text[lowIndex], ref text[highIndex]);
+                }
+                else return highIndex;
+
+            }
+        }
+        public static void Swap(ref char x, ref char y)
+        {
+            var temp = y;
+            y = x;
+            x = temp;
+        }
+
+        public static string GetDescendingText(char[] text, int lowIndex, int highIndex)
+        {
+            QuickSortForOrderAscendingText(text, lowIndex, highIndex);
+            var textDescending=text.Reverse();
+            return  ToArrayString(textDescending);
+
+        }
+
+        public static string ToArrayString(IEnumerable<char> charSequence)
+        {
+            return new string(charSequence.ToArray());
+        }
+
+        public static char[] BubbleSort(char[] number)
+        {
+            var isSorted = false;
+            while (!isSorted) 
+            {
+                isSorted = true;
+                for (var i = 0; i < number.Length - 1; i++)
+                {
+                    if (number[i] > number[i + 1])
+                    {
+                        Swap(ref number[i], ref number[i + 1]);
+                        isSorted = false;
+                    }
+                }
+            }
+            return number;
+        }
+
+        //public static void QuickSort3Way(string[] priority, int lowIndex, int highIndex)
+        //{
+        //    if (highIndex <= lowIndex) return;
+
+        //    var i = lowIndex + 1;
+        //    var pivot = priority[lowIndex];
+
+        //    while (i <= highIndex)
+        //    {
+        //        if (Less(priority[i],pivot))
+        //            Swap(priority[i++], priority[lowIndex++]);
+        //        if(Less(pivot, input[i]))
+        //            Swap(priority[i], priority[higtIndex--]);
+        //        else i++;
+        //    }
+        //   QuickSort3Way(priority, lowIndex, lowIndex-1);
+        //   QuickSort3Way(priority, highIndex+1, highIndex);
+        //}
+
+        public static bool Less(string x, string y)
+        {
+            var isLess = false;
+            for (var i = 0; i < x.Length; i++)
+                if (x[i] <= y[i])
+                    isLess = true;
+                else    
+                {
+                    isLess = false;
+                    break;
+                }
+            return isLess;
+        }
+    }
+}
