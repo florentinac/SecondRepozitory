eeb9cfe7b2504b2aebaeb8e2c46934a213b5619c
florentinc <florentina.cociota@gfi.com>
2015-11-04 09:01:42 +0200

    Add OOP project

---
+++ MyOOP/MyOOP/ArrayClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Array;
+
+namespace MyOOP
+{
+    public class ArrayClass
+    {
+        public ArrayClass(object[] data)
+        {
+            this._data = data;
+        }
+
+        private object[] _data;       
+        private int _count;
+
+        public object[] Add(object newElement)
+        {
+            var newArray = new object[8];
+
+            if (_data.Length > newArray.Length)          
+                Resize(ref newArray, 2 * newArray.Length);
+
+            Copy(_data, newArray, _data.Length);
+            newArray[_data.Length] = newElement;
+
+            return newArray;
+        }
+
+
+    }
+}
---
+++ MyOOP/MyOOPTests/TestVector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyOOP;
+
+namespace MyOOPTests
+{
+    [TestClass]
+    public class TestVector
+    {
+        [TestMethod]
+        public void AddIntElementInObjectArray()
+        {
+            var intArray = new object[] { 1, 2, 3 };
+            var newElement = 1;
+            var expectedResult = new object[] {1, 2, 3, 1, null, null, null, null};
+            var array = new ArrayClass(intArray);
+            var newArray = array.Add(newElement);
+
+            CollectionAssert.AreEqual(expectedResult,newArray);
+        }
+
+        [TestMethod]
+        public void AddStringElementInObjectArray()
+        {
+            var intArray = new object[] { "ana", "are", "mere" };
+            var newElement = "pere";
+            var expectedResult = new object[] { "ana", "are", "mere", "pere", null, null, null, null };
+            var array = new ArrayClass(intArray);
+            var newArray = array.Add(newElement);
+
+            CollectionAssert.AreEqual(expectedResult, newArray);
+        }
+    }
+}
