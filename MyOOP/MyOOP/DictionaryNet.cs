93de2674a851aef7b5352f644cfac75f3db79c49
florentinc <florentina.cociota@gfi.com>
2015-11-19 17:07:28 +0200

    Add DictionaryNet Method and test for Empty Dictionary

---
+++ MyOOP/MyOOP/DictionaryNet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyOOP
+{
+    public class DictionaryNet<Key, T>:ICollection
+    {
+        private int[] buckets= new int[50];
+        private Entry[] entries = new Entry[50];
+
+        private class Entry
+        {
+            public int next;
+            public int indexer;
+            public Key key;
+            public T value;
+
+            public Entry(int indexer, int next)
+            {
+                this.indexer = indexer;
+                this.next = next;
+            }
+        }
+
+        public DictionaryNet()
+        {
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Entry entry in entries)
+            {
+                if(entry!=null)
+                yield return entry;
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public object SyncRoot { get; }
+        public bool IsSynchronized { get; }
+    }
+}
---
+++ MyOOP/MyOOPTests/Dictionary.NetTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyOOP;
+using Should;
+
+namespace MyOOPTests
+{
+    [TestClass]
+    public class Dictionary
+    {
+        [TestMethod]
+        public void VerfiyAnEmptyDictionary()
+        {
+            var dictionary = new DictionaryNet<string,string>();
+            dictionary.ShouldBeEmpty();
+        }
+    }
+}
